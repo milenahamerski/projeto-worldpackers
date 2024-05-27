1d62e91dc84a4a5ab238177a30a5ef183a8f4483
Milena Hamerski <milena.h9@hotmail.com>
2024-05-27 18:42:18 -0300

    update de fundo e tela de login

---
+++ assets/main/tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          100: "#01585D",
+          200: "#027F86",
+        },
+        gray: "#D8D0C5",
+        chalk: "#333737",
+        "ocre-light": "#D6B670",
+      },
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        background: 'url("/projeto-worldpackers/assets/img/background.png")',
+        sea: 'url("/projeto-worldpackers/assets/img/sea.jpg")',
+        logo: 'url("/projeto-worldpackers/assets/img/seashellther.png")',
+      },
+      fontSize: {
+        "2xl": "2.0rem",
+      },
+    },
+  },
+};
